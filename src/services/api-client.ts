34f210c2c80de2ee413dbbdbcc18d8f6fa249ce3
Mohamed Atta <M.TarekSaad@nu.edu.eg>
2024-10-09 14:04:11 +0300

    Final Commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: '85f16b6cf1814ba5b1cd917dbc60fc65'
-  }
-})
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
